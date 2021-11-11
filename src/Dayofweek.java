abe85b3c431cf97e961499c89a61a43b06a9899c
Ganga <ganga_si@gmail.com>
2021-11-11 19:43:13 +0530

    New Javafiles

---
+++ src/Dayofweek.java
@@ -0,0 +1,30 @@
+import com.sun.xml.internal.xsom.XSComplexType;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
+public class Dayofweek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+        //int a = sc.nextInt();
+        int a = 5;
+        int b = 8;
+        int  c = 2;
+        int d = 3;
+        int smallest = 0;
+        if (a < b && a < c && a < d){
+            System.out.println("smallest = a");
+        }
+        if (b<a && b < c && b < d) {
+            System.out.println("smallest = b");
+        }
+        if (c < a && c < b && c < d) {
+            System.out.println("smallest = c");
+        }
+        if (d < a && d < c && d < b){
+            System.out.println("smallest = d");
+    }
+
+
+    }
+}
---
+++ src/Printmessage.java
@@ -0,0 +1,17 @@
+public class Printmessage{
+    //single line comment
+    public static void main(String[] args) {
+         String message="I  rocking";
+         boolean bool=false;
+         int age=24;
+         char ch='F';
+        System.out.println(message);
+        //Add two variables
+        float var1=2.0f;
+        int var2=5;
+        float output=var1+var2;
+        System.out.println(output);
+        /* larger comments written*/
+    }
+
+}
