a46a4bf36b8f6ac546b2455a36c06e44fe0e140d
Ganga <ganga_si@gmail.com>
2021-11-20 11:28:20 +0530

    Done

--- src/Day2/Calculator.java
+++ src/Day2/Calculator.java
@@ -1,57 +1,59 @@
+package Day2;
+
 import java.util.Scanner;
 
 public class Calculator {
         public static void main(String[] args) {
 
-            char operator;
+            char operator='A';
             double number1, number2, result;
+             while(operator!='E') {
+                 // create an object of Scanner class
+                 Scanner input = new Scanner(System.in);
 
-            // create an object of Scanner class
-            Scanner input = new Scanner(System.in);
+                 // ask users to enter numbers
+                 System.out.println("Enter first number");
+                 number1 = input.nextDouble();
 
-            // ask users to enter numbers
-            System.out.println("Enter first number");
-            number1 = input.nextDouble();
+                 System.out.println("Enter second number");
+                 number2 = input.nextDouble();
 
-            System.out.println("Enter second number");
-            number2 = input.nextDouble();
-
-            // ask users to enter operator
-            System.out.println("Choose an operator: +, -, *, or /");
-            operator = input.next().charAt(0);
+                 // ask users to enter operator
+                 System.out.println("Choose an operator: +, -, *, /,E");
+                 operator = input.next().charAt(0);
 
 
-            switch (operator) {
+                 switch (operator) {
 
-                // performs addition between numbers
-                case '+':
-                    result = number1 + number2;
-                    System.out.println(number1 + " + " + number2 + " = " + result);
-                    break;
+                     // performs addition between numbers
+                     case '+':
+                         result = number1 + number2;
+                         System.out.println(number1 + " + " + number2 + " = " + result);
+                         break;
 
-                // performs subtraction between numbers
-                case '-':
-                    result = number1 - number2;
-                    System.out.println(number1 + " - " + number2 + " = " + result);
-                    break;
+                     // performs subtraction between numbers
+                     case '-':
+                         result = number1 - number2;
+                         System.out.println(number1 + " - " + number2 + " = " + result);
+                         break;
 
-                // performs multiplication between numbers
-                case '*':
-                    result = number1 * number2;
-                    System.out.println(number1 + " * " + number2 + " = " + result);
-                    break;
+                     // performs multiplication between numbers
+                     case '*':
+                         result = number1 * number2;
+                         System.out.println(number1 + " * " + number2 + " = " + result);
+                         break;
 
-                // performs division between numbers
-                case '/':
-                    result = number1 / number2;
-                    System.out.println(number1 + " / " + number2 + " = " + result);
-                    break;
+                     // performs division between numbers
+                     case '/':
+                         result = number1 / number2;
+                         System.out.println(number1 + " / " + number2 + " = " + result);
+                         break;
 
-                default:
-                    System.out.println("Invalid operator!");
-                    break;
-            }
-
+                     case 'E':
+                         System.out.println("Invalid operator!");
+                         break;
+                 }
+             }
         }
 
 }
