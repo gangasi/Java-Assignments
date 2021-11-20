33d37c635c8547a9d0378ae2bf31223d4ebed32f
Ganga <48200544+gangasi@users.noreply.github.com>
2021-11-20 14:33:37 +0530

    Inheritance Assignments

---
+++ src/KidRoom.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+    public class KidRoom extends Home{
+        KidRoom(int noOfLights){
+            this.noOfLights=noOfLights;
+            this.room="Kid room";
+        }
+
+    }
