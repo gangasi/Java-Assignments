4faf160b2dbdaaca495e8b45ff3ead6a12149585
Ganga <48200544+gangasi@users.noreply.github.com>
2021-11-20 14:56:04 +0530

    Polymorphism-1

--- src/methodoverloading.java
+++ src/methodoverloading.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class methodoverloading {
+import java.util.Scanner;
+
+public class methodoverloading {
+    public static void main(String[] args) {
+
+        //Adding 2 numbers
+        Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        float sum=addNumbers(a,b);
+        System.out.println("Sum is:" +sum);
+
+        //Adding third number
+        System.out.println("Enter Third number:");
+        float c = sc.nextFloat();
+         sum=addNumbers(a,b,c);
+        System.out.println("Sum is:" +sum);
+
+    }
+
+
+
+    static int addNumbers(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+    static float addNumbers(int a,int b,float c){
+        float sum=a+b+c;
+        return sum;
+    }
 }
